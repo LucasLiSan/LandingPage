9c02cf3bb7a5fe2eb04200c35ee85836cadd4895
LucasLiSan <lukassantana_rgt@hotmail.com>
2023-04-24 17:24:14 -0300

    textos

--- _assets/_js/values.js
+++ _assets/_js/values.js
@@ -1,24 +1,17 @@
-var $animation_elements = $('.animation-element');
-var $window = $(window);
+window.addEventListener('scroll', reveal);
 
-function check_if_in_view() {
-    var window_height = $window.height();
-    var window_top_position = $window.scrollTop();
-    var window_bottom_position = (window_top_position + window_height);
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
 
-    $.each($animation_elements, function() {
-        var $element = $(this);
-        var element_height = $element.outerHeight();
-        var element_top_position = $element.offset().top;
-        var element_bottom_position = (element_top_position + element_height);
+    for(var i = 0; i < reveals.length; i++){
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
 
-        if ((element_bottom_position >= window_top_position) && (element_top_position <= window_bottom_position)) {
-            $element.addClass('in-view');
-        } else {
-            $element.removeClass('in-view');
+        if(revealtop < windowheight - revealpoint){
+            reveals[i].classList.add('active');
+        } else{
+            reveals[i].classList.remove('active');
         }
-    });
+    }
 }
-
-$window.on('scroll resize', check_if_in_view);
-$window.trigger('scroll');
